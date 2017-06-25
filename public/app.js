c1ac759db2a5675234112cb6fd446195c0f302a6
into <mirakuru.m@yandex.ru>
2017-06-25 20:33:39 +0400

    update orders input

--- public/app.js
+++ public/app.js
@@ -15,6 +15,14 @@ function add_to_cart(id) {
     x = x * 1 + 1;
     window.localStorage.setItem(key, x);
 
+    update_orders_input();
+
+}
+
+function update_orders_input() {
+    var orders = cart_get_orders();
+    $('#orders_input').val(orders);
+    
 }
 
 function cart_get_number_of_items() {
@@ -31,4 +39,20 @@ function cart_get_number_of_items() {
         }
     }
     return cnt;
+}
+
+function cart_get_orders() {
+    var orders = '';
+
+    for (var i = 0; i < window.localStorage.length; i++)
+    {
+        var key = window.localStorage.key(i);
+        var value = window.localStorage.getItem(key);
+
+        if(key.indexOf('product_') == 0)
+        {
+            orders = orders + key + '=' + value + ',';
+        }
+    }
+    return orders;
 }
