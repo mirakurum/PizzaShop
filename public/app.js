4f3284f05365be7fa70aec5f983058ec1c5b79d8
into <mirakuru.m@yandex.ru>
2017-06-25 19:31:37 +0400

    add pizza in cart counter

--- public/app.js
+++ public/app.js
@@ -8,8 +8,27 @@ function something() {
     alert(x);
 }
 
-function add_to_cart() {
+function add_to_cart(id) {
+    var key ='product_' + id;
 
-    alert('Hi');
+    var x = window.localStorage.getItem(key);
+    x = x * 1 + 1;
+    window.localStorage.setItem(key, x);
 
+}
+
+function cart_get_number_of_items() {
+    var cnt = 0;
+
+    for (var i = 0; i < window.localStorage.length; i++)
+    {
+        var key = window.localStorage.key(i);
+        var value = window.localStorage.getItem(key);
+
+        if(key.indexOf('product_') == 0)
+        {
+            cnt = cnt + value * 1;
+        }
+    }
+    return cnt;
 }
